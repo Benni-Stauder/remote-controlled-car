665cb4b59ae85a7b2387ec7332399777bbe57a7d
annikajung <annika.jung13@gmail.com>
2024-05-07 21:09:00 +0200

    websocket can send and recieve data

--- ws-server/server.py
+++ ws-server/server.py
@@ -3,20 +3,53 @@ import websockets
 import json
 import random
 
-async def handler(websocket, path):
+
+async def send_data(websocket):
     while True:
         try:
-            # Erzeugt einen zufälligen Geschwindigkeitswert
             speed = random.randint(20, 100)
-            # Verpackt den Geschwindigkeitswert in ein JSON-Format
-            message = json.dumps({"speed": speed})
-            await websocket.send(message)  # Sendet die JSON-formatierte Nachricht an den Client
-            await asyncio.sleep(1)
-            print("hier")# Warte für 1 Sekunde vor dem Senden der nächsten Nachricht
-        except websockets.ConnectionClosed:
-            break  # Beendet die Schleife, wenn der Client die Verbindung trennt
+            rpm = random.randint(1000, 8000)
+            lateral_acceleration = random.uniform(0.0, 1.0)
+            brake_percentage = random.randint(0, 100)
 
-# Startet den WebSocket-Server
+            message = json.dumps({
+                "speed": speed,
+                "rpm": rpm,
+                "lateralAcceleration": lateral_acceleration,
+                "brakePercentage": brake_percentage
+            })
+
+            await websocket.send(message)
+
+
+        except websockets.ConnectionClosed:
+            print("Connection closed, stopping data sender.")
+            break
+
+
+async def receive_data(websocket):
+    async for message in websocket:
+        print("Received message from client:", message.maxSpeed)
+        message_from_client = json.dumps({
+            "maxSpeed": message.maxSpeed,
+            "assistance": message.assistance,
+            "drivingMode": message.drivingMode,
+        })
+
+
+async def handler(websocket, path):
+    sender_task = asyncio.ensure_future(send_data(websocket))
+    receiver_task = asyncio.ensure_future(receive_data(websocket))
+
+    done, pending = await asyncio.wait(
+        [sender_task, receiver_task],
+        return_when=asyncio.FIRST_COMPLETED,
+    )
+
+    for task in pending:
+        task.cancel()
+
+
 start_server = websockets.serve(handler, "localhost", 8000)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
